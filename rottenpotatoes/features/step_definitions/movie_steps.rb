404d758daf70d6fd7382c30877e9c26b29bb24a7
Om Kanwar <okanwar@sandiego.edu>
2018-10-09 00:08:16 +0000

    done

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -9,6 +9,11 @@ Given /the following movies exist/ do |movies_table|
   #fail "Unimplemented"
 end
 
+Then /I should see (\d+) movies$/ do |movie_count|
+  movies_shown = page.all("#movies tbody td:nth-child(1)").map { |m| m.text }
+  movies_shown.count.should == movie_count.to_i
+end
+
 Then /(.*) seed movies should exist/ do | n_seeds |
   Movie.count.should be n_seeds.to_i
 end
@@ -17,10 +22,14 @@ end
 #   on the same page
 
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
+    movies_shown = page.all("#movies tbody td:nth-child(1)").map { |m| m.text }
+    
+   (movies_shown.index("#{e1}") < movies_shown.index("#{e2}")).should be true
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
   #fail "Unimplemented"
 end
+
 
 # Make it easier to express checking or unchecking several boxes at once
 #  "When I uncheck the following ratings: PG, G, R"
@@ -30,10 +39,19 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-  fail "Unimplemented"
+   rating_list.split(/\s*,\s*/).each do |rating|
+    if uncheck.nil?
+      step %Q{I check "ratings_#{rating}"}
+    else
+      step %Q{I uncheck "ratings_#{rating}"}
+    end
+   end
+  #fail "Unimplemented"
 end
 
 Then /I should see all the movies/ do
+   movies_shown = page.all("#movies tbody td:nth-child(1)").map { |m| m.text }
+   movies_shown.count.should == Movie.count
   # Make sure that all the movies in the app are visible in the table
-  fail "Unimplemented"
+  #fail "Unimplemented"
 end
